0c78411c3bf6ac4878f5c7136ca51af00dc10c92
parth-ka-patel <parth.kapatel@arsenaltech.com>
2021-09-06 18:30:27 +0530

    Use this for directly child select or in tab section

--- src/OptionsController.php
+++ src/OptionsController.php
@@ -14,9 +14,26 @@ class OptionsController extends Controller
 
         $resource = $request->newResource();
         $fields = $resource->updateFields($request);
-        $field = $fields->findFieldByAttribute($attribute);
+        $field = $this->findFieldByAttribute($fields,$attribute);
         $options = $field->getOptions($parentValue);
 
         return $options;
+    }
+
+    public function findFieldByAttribute($fieldObj,$attribute){
+        foreach ($fieldObj as $field){
+            if(gettype($field) == "array" && $field["component"] == "tabs"){
+                foreach ($field["fields"] as $value){
+                    if (isset($value->attribute) && $value->attribute == $attribute && $value->component == "child-select"){
+                        return $value;
+                    }
+                }
+            }else{
+                if(isset($field->attribute) &&
+                    $field->attribute == $attribute){
+                    return $field;
+                }
+            }
+        }
     }
 }
